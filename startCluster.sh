5099afb4e486bc41b8636d01e80868fc9fc3afbd
dustedduke <dustedduke@gmail.com>
2019-06-12 11:52:45 +0300

    env fixed

--- startCluster.sh
+++ startCluster.sh
@@ -12,7 +12,7 @@ docker-compose up -d --scale spark-worker=$N
 #docker run -v `pwd`:/data --name hadoop --hostname hadoop --network sparkNet -p 50070:50070 -p 9000:9000 -p 50075:50075 -p 50010:50010 #teivah/hadoop:2.9.2
 docker exec -it hadoop-hdfs data/init-hadoop.sh
 
-docker run -it -e SPARK_MASTER="spark://spark-master:7077" -v `pwd`:/local --network sparkNet dustedduke/spark:latest /bin/bash
+docker run -it -e SPARK_MASTER="spark://spark-master:7077" -e PYSPARK_PYTHON=/usr/bin/python3.6 -v `pwd`:/local --network sparkNet dustedduke/spark:latest /bin/bash
 
 
 
